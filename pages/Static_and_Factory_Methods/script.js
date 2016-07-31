909bacf080c846aa241fb7976b01138efc011b01
Ainur Farkh <farkhutdinov1398@gmail.com>
2016-07-31 13:15:20 +0300

    Learned static methods

--- pages/Static_and_Factory_Methods/script.js
+++ pages/Static_and_Factory_Methods/script.js
@@ -1,5 +1,6 @@
 "use strict"
 
+/*
 function Article() {
   Article.count++;
 } 
@@ -14,3 +15,39 @@ Article.showCount = function() {
 new Article();
 new Article();
 Article.showCount();
+*/
+
+function Journal(date) {
+  this.date = date;
+
+  this.getTitle = function() {
+    return "The release of " + Journal.formatDate(this.date);
+  };
+} 
+
+Journal.compare = function(journalA, journalB) {
+  return journalA.date - journalB.date;
+};
+
+Journal.formatDate = function(date) {
+  return date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+};
+
+var journals = [
+  new Journal(new Date(2012, 1, 1)),
+  new Journal(new Date(2012, 0, 1)),
+  new Journal(new Date(2011, 11, 1)),
+];
+
+function findMin(journals) {
+  var min = 0;
+  for (var i = 0; i < journals.length; i++) {
+    if (Journal.compare(journals[min], journals[i]) > 0) min = i;
+  } 
+  return journals[min];
+}
+
+//alert(findMin(journals).getTitle());
+
+//F A C T O R Y  M E T H O D S
+
